c71cbbe102f7fae0a5ffd47d84fd29f874feec86
kendrick010 <nguyen.kendrick@yahoo.com>
2022-02-16 02:19:50 -0800

    hardware setup, sensor data

---
+++ hardware/LED.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+ledPin = 18
+   
+def setup():
+    GPIO.setmode(GPIO.BCM)     
+    GPIO.setup(ledPin, GPIO.OUT)    # set ledPin to OUTPUT mode
+
+def toggleOn():
+    GPIO.output(ledPin, GPIO.HIGH)
+
+def toggleOff():
+    GPIO.output(ledPin, GPIO.LOW)
+
+def loop():
+    while(True):
+        toggleOn()
+        time.sleep(1)
+        toggleOff()
+        time.sleep(1)
+
+if __name__ == '__main__':     # Program entrance
+    print ('Program is starting...')
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:  # Press CTRL-C to end the program
+        GPIO.cleanup()         # release GPIO resources
